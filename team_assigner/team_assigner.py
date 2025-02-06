2c9caad6ecf04f26a32dddaa66a126568d5da2b5
Sou7ai1 <m.souhail03@gmail.com>
2025-02-06 15:40:32 +0100

    Improve Clustering and Team color prediction

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 import numpy as np
+import cv2  # type: ignore
 
 
 class TeamAssigner:
@@ -14,19 +15,35 @@ class TeamAssigner:
         return kmeans
 
     def get_player_color(self, frame, bbox):
+        # Ensure the bounding box is valid
+        if bbox[0] >= bbox[2] or bbox[1] >= bbox[3]:
+            raise ValueError("Invalid bounding box coordinates")
+
         image = frame[int(bbox[1]): int(bbox[3]), int(bbox[0]): int(bbox[2])]
 
+        image = cv2.GaussianBlur(image, (5, 5), 0)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+
         top_player = image[0:int(image.shape[0]/2), :]
-        kmeans = self.get_cluster_model(top_player)
+
+        pixels = top_player.reshape(-1, 3)
+        kmeans = KMeans(n_clusters=2, init='k-means++', n_init=10)
+        kmeans.fit(pixels)
+
+        # Determine which cluster corresponds to the player
         labels = kmeans.labels_
         clustered_image = labels.reshape(
             top_player.shape[0], top_player.shape[1])
         corner_cluster = [clustered_image[0, 0], clustered_image[0, -1],
                           clustered_image[-1, 0], clustered_image[-1, -1]]
         non_player_cluster = max(set(corner_cluster), key=corner_cluster.count)
-        player_cluster = 1-non_player_cluster
+        player_cluster = 1 - non_player_cluster
 
+        # Get the dominant color of the player
         player_color = kmeans.cluster_centers_[player_cluster]
+        player_color = cv2.cvtColor(
+            np.uint8([[player_color]]), cv2.COLOR_LAB2BGR)[0][0]
+
         return player_color
 
     def get_team_player(self, frame, player_bbox, player_id):
@@ -34,17 +51,12 @@ class TeamAssigner:
             return self.team_player[player_id]["team"]
 
         player_color = self.get_player_color(frame, player_bbox)
-
-        # Validate player_color
-        if np.isnan(player_color).any() or np.all(player_color == 0):
-            # Assign a default team (e.g., team 1) if the color is invalid
+        if not np.isfinite(player_color).all() or np.linalg.norm(player_color) == 0:
             team_id = 1
         else:
-            # Predict the team using the KMeans model
-            team_id = self.kmeans.predict(player_color.reshape(1, -1))[0]
-            team_id += 1  # Teams are 1 and 2
+            team_id = self.kmeans.predict(player_color.reshape(1, -1))[0] + 1
 
-        # Store the team assignment and player color
+        # Cache player data
         self.team_player[player_id] = {
             "team": team_id,
             "color": player_color
