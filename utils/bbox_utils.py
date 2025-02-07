364181b5111db9c6574f13ad30332ed5bf0b035c
Sou7ai1 <m.souhail03@gmail.com>
2025-02-07 14:36:56 +0100

    Add Measure Distance

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -1,8 +1,10 @@
 def get_center_box(bbox):
     return int((bbox[0] + bbox[2]) / 2)
 
+
 def get_bbox_width(bbox):
     return int(bbox[2] - bbox[0])
 
-def measure_distance (pos1,pos2):
-    return ((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2) ** 0.5
+
+def measure_distance(pos1, pos2):
+    return ((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2)**0.5
