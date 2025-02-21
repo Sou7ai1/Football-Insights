496b1e9ee613f6d67193072d66c481df543c727c
Sou7ai1 <m.souhail03@gmail.com>
2025-02-21 20:31:41 +0100

    Draw Camera Movement

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -34,10 +34,10 @@ class CameraMovementestimator():
 
     def get_camera_movement(self, frames, read_from=False, stub_path=None):
 
-        if read_from and stub_path is not None and os.path.exists(stub_path): 
+        if read_from and stub_path is not None and os.path.exists(stub_path):
             with open(read_from, 'rb') as f:
                 return pickle.load(f)
-        
+
         camera_movement = [[0, 0]*len(frames)]
         old_color = cv2.cvtColor(frames[0], cv2.COLOR_BGR2GRAY)
         old_features = cv2.goodFeaturesToTrack(old_color, **self.features)
@@ -56,17 +56,38 @@ class CameraMovementestimator():
                     max_dis = distance
                     camera_movement_x, camera_movement_y = measure_xy_distance(
                         old_features_p, new_features_p)
-                    
+
             if max_dis < self.min_distance:
                 camera_movement[frame_N] = [
                     camera_movement_x, camera_movement_y]
                 old_features = cv2.goodFeaturesToTrack(
                     frame_gray, **self.features)
-                
+
             old_color = frame_gray.copy()
-        
+
         if stub_path:
             with open(stub_path, 'wb') as f:
                 pickle.dump(camera_movement)
-            
+
         return camera_movement
+
+    def draw_camera_movement(self, frames, camera_movement):
+        output_frames = []
+
+        for frame_N, frame in enumerate(frames):
+            frame = frame.copy()
+            overlay = frame.copy()
+            cv2.rectangle(overlay, (0, 0), (500, 100), (255, 255, 255), -1)
+            alpha = 0.6  # for transparency
+            cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+
+            movement_x, movement_y = camera_movement[frame_N]
+            frame = cv2.putText(frame, f"Camera Movement X: {movement_x:.2f}", (
+                10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+            frame = cv2.putText(frame, f"Camera Movement Y: {movement_y:.2f}", (
+                10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+            
+            
+            output_frames.append(frame)
+            
+        return output_frames
